2739fd025e91314bb3cbf4f000091af7aa6739fa
Irina B <bl00dyhll@yahoo.com>
2020-07-19 14:30:27 +0300

    test push

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {HomePage} from './components/homepage.component';
+
+function App() {
+  
+  return (
+    <div>
+      <HomePage />
+      <div>sklnadlka</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/homepage.component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+export const HomePage = () => {
+   return (
+       <div className='homepage'>
+           <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>HATS</h1>
+                        <span className='subtitle'>section description</span>
+                    </div>
+                </div>
+           </div>
+       </div>
+   ) 
+}
