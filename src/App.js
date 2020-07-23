907afc6e3d55e1b36e9f450511f72718e5caa5d7
Irina B <bl00dyhll@yahoo.com>
2020-07-23 20:20:16 +0300

    added header + style

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 class App extends React.Component {
@@ -16,6 +17,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<Header />
 				<Switch>
 					<Route exact path='/' component={ HomePage } />
 					<Route exact path='/shop' component={ ShopPage } />
