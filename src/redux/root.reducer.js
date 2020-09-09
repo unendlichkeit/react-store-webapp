94555e9cfa6445c75a31c6318c151344ebfb5741
Irina B <bl00dyhll@yahoo.com>
2020-09-09 21:29:15 +0300

    root.reducer modified?!?

--- src/redux/root.reducer.js
+++ src/redux/root.reducer.js
@@ -4,5 +4,6 @@ import cartReducer from './cart/cart.reducer';
 
 export default combineReducers({
     user: userReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    // shop: shopReducer
 })
