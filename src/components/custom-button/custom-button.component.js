616d4d464901a5d8ed78e9b17d4a0ee1d7d61cba
Irina B <bl00dyhll@yahoo.com>
2020-09-09 08:08:59 +0300

    added everything to git

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomBtn = ({ children, isGoogleSignIn, ...otherProps}) => (
-    <button className={`${isGoogleSignIn ? isGoogleSignIn : ''} custom-button`} {...otherProps}>
+const CustomBtn = ({ children, isGoogleSignIn, inverted, ...otherProps}) => (
+    <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? isGoogleSignIn : ''} custom-button`} {...otherProps}>
         {children}
     </button>
 );
