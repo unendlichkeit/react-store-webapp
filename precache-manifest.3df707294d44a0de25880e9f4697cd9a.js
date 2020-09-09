cfaa129138eb9d8875d7a85464f1c06daabfd37d
Irina B <bl00dyhll@yahoo.com>
2020-09-09 20:07:34 +0300

    Updates

---
+++ precache-manifest.3df707294d44a0de25880e9f4697cd9a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1862209352833a21f43db5141f36ed0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8cbc3af13441dc9f346",
+    "url": "/static/css/main.2e86cdc8.chunk.css"
+  },
+  {
+    "revision": "9ca044c2b66c0f2aa246",
+    "url": "/static/js/2.795c8642.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/static/js/2.795c8642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cbc3af13441dc9f346",
+    "url": "/static/js/main.8e43c23d.chunk.js"
+  },
+  {
+    "revision": "939919d6a7570754d879",
+    "url": "/static/js/runtime-main.656d09a4.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/static/media/crown-logo.19ef7b17.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-store-webapp/precache-manifest.0c363ef4d2ac7d724364477f8c378971.js"
+  "/precache-manifest.3df707294d44a0de25880e9f4697cd9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-store-webapp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
