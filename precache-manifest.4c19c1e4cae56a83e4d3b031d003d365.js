8c1e8c915dcab6f24d77d95ace4b6e3a0c5df2b5
Irina B <bl00dyhll@yahoo.com>
2020-09-17 08:43:00 +0300

    Updates

--- precache-manifest.4c19c1e4cae56a83e4d3b031d003d365.js
+++ precache-manifest.4c19c1e4cae56a83e4d3b031d003d365.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "934c9d02e900c730ec87ff5ebbdd3dcb",
+    "revision": "dff77e4c7e5fb9bea8aa10e459650ea7",
     "url": "/react-store-webapp/index.html"
   },
   {
-    "revision": "b1979ae2299ef3f05d7e",
+    "revision": "1ca460c8f4392a4ad798",
     "url": "/react-store-webapp/static/css/main.2e86cdc8.chunk.css"
   },
   {
-    "revision": "164d4eae3e777c7a5c7f",
-    "url": "/react-store-webapp/static/js/2.34596eb5.chunk.js"
+    "revision": "6535362e42f4b457ab41",
+    "url": "/react-store-webapp/static/js/2.e2a0a8ea.chunk.js"
   },
   {
     "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
-    "url": "/react-store-webapp/static/js/2.34596eb5.chunk.js.LICENSE.txt"
+    "url": "/react-store-webapp/static/js/2.e2a0a8ea.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b1979ae2299ef3f05d7e",
-    "url": "/react-store-webapp/static/js/main.ba2ccd9b.chunk.js"
+    "revision": "1ca460c8f4392a4ad798",
+    "url": "/react-store-webapp/static/js/main.7471eb8f.chunk.js"
   },
   {
     "revision": "597252985bd45fdca2e8",
